84d1dfd3fcb99bc03f8feb2a67608e3686a144f2
Александр Белёв <belyov97@gmail.com>
2017-11-24 15:55:23 +0500

    tables creation

--- 03/lab3.sql
+++ 03/lab3.sql
@@ -34,24 +34,114 @@ GO
 CREATE SCHEMA lab3 
 GO
 
+IF OBJECT_ID('[Belyov].lab3.Teams', 'U') IS NOT NULL
+  DROP TABLE  [Belyov].lab3.Teams
+GO
+
+IF OBJECT_ID('[Belyov].lab3.Players', 'U') IS NOT NULL
+  DROP TABLE  [Belyov].lab3.Players
+GO
+
 IF OBJECT_ID('[Belyov].lab3.GamesResults', 'U') IS NOT NULL
   DROP TABLE  [Belyov].lab3.GamesResults
 GO
 
-CREATE TABLE lab3.GamesResults
+IF OBJECT_ID('[Belyov].lab3.Goals', 'U') IS NOT NULL
+  DROP TABLE  [Belyov].lab3.Goals
+GO
+
+CREATE TABLE lab3.Teams
 (
-    GameId int IDENTITY(1,1) NOT NULL,
-    HostTeam NVARCHAR(40) NOT NULL,
-    GuestTeam NVARCHAR(40) NOT NULL,
-    GameDate DATETIME NOT NULL,
-    Score NVARCHAR(10) NOT NULL,
-    HostGoalAuth NVARCHAR(MAX) NOT NULL,
-    GuestGoalAuth NVARCHAR(MAX) NOT NULL,
-    HostGoalKeeper NVARCHAR(40) NOT NULL,
-    GuestGoalKeeper NVARCHAR(40) NOT NULL
+  TeamId int IDENTITY(1,1) NOT NULL,
+  TeamName nvarchar(40) not null,
+  CONSTRAINT PK_TeamId PRIMARY KEY(TeamId)
 )
 
-INSERT INTO lab3.GamesResults VALUES
-('Спартак', 'Урал', '2017-11-24', '0-3', '', 'Иванов, Петров, Сидоров', 'Спартаквртр', 'Уралвртр')
+INSERT INTO lab3.Teams
+VALUES
+  ('Спартак'),
+  ('Урал')
 
-SELECT * FROM lab3.GamesResults
+CREATE TABLE lab3.Players
+(
+  PlayerId int IDENTITY(1,1) not null,
+  TeamId int not null,
+  PlayerSurname nvarchar(40) not null,
+  PlayerRole nvarchar(15) not null,
+  CONSTRAINT PK_PlayerId PRIMARY KEY(PlayerId),
+  CONSTRAINT FK_TeamId FOREIGN KEY (TeamId)
+  REFERENCES lab3.Teams(TeamId)
+  ON UPDATE CASCADE
+)
+
+INSERT INTO lab3.Players
+VALUES
+  (1, 'Давыдов', 'Нападающий'),
+  (1, 'Адриано', 'Нападающий'),
+  (1, 'Ребров', 'Вратарь'),
+  (2, 'Ильин', 'Нападающий'),
+  (2, 'Арапов', 'Вратарь')
+
+
+CREATE TABLE lab3.GamesResults
+(
+  GameId int IDENTITY(1,1) NOT NULL,
+  HostTeamId int NOT NULL,
+  GuestTeamId int NOT NULL,
+  GameDate DATETIME NOT NULL,
+  HostScore tinyint not null,
+  GuestScore tinyint not null,
+  CONSTRAINT PK_GameId PRIMARY KEY(GameId),
+  CONSTRAINT FK_HostTeamId FOREIGN KEY (HostTeamId)
+    REFERENCES lab3.Teams(TeamId)
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_GuestTeamId FOREIGN KEY (GuestTeamId)
+    REFERENCES lab3.Teams(TeamId)
+    ON UPDATE NO ACTION
+)
+
+INSERT INTO lab3.GamesResults
+VALUES
+  (1, 2, '2017-11-10', 3, 0),
+  (2, 1, '2017-11-11', 2, 1)
+
+CREATE TABLE lab3.Goals
+(
+  GameId int not null,
+  PlayerId int not null,
+  CONSTRAINT FK_GameId FOREIGN KEY (GameId)
+  REFERENCES lab3.GamesResults(GameId)
+  ON UPDATE CASCADE,
+  CONSTRAINT FK_PlayerId FOREIGN KEY (PlayerId)
+  REFERENCES lab3.Players(PlayerId)
+  ON UPDATE CASCADE
+)
+
+INSERT INTO lab3.Goals
+VALUES
+  (1, 1),
+  (1, 2),
+  (1, 2),
+  (2, 4),
+  (2, 4),
+  (2, 1)
+
+
+SELECT *
+FROM lab3.GamesResults
+GO
+
+CREATE FUNCTION lab3.TournamentResults(@inpDate DATETIME)
+RETURNS @retResults TABLE
+(
+  Place INT,
+  Name nvarchar(40),
+  Points int,
+  Goals int,
+  LosesGoals int
+)
+AS
+BEGIN
+  RETURN;
+END
+GO
