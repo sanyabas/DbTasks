983f3cddf1b95f935a574db92bf4299814b1f6a6
Александр Белёв <belyov97@gmail.com>
2017-12-21 20:23:27 +0500

    segment boundaries fix

--- 04/lab4.sql
+++ 04/lab4.sql
@@ -47,17 +47,20 @@ CREATE TABLE lab4.Tariffs(
 )
 
 INSERT INTO lab4.Tariffs VALUES
-('Без абонентской платы', 0, 0, 2),
-('XS', 150, 100, 3),
-('S', 250, 200, 3),
-('M', 400, 450, 4),
-('L', 700, 800, 5),
-('Безлимитный', 1000, 1200, 0)
+-- ('Без абонентской платы', 0, 0, 2),
+-- ('XS', 150, 100, 3),
+-- ('S', 250, 200, 3),
+-- ('M', 400, 450, 4),
+-- ('L', 700, 800, 5),
+-- ('Безлимитный', 1000, 1200, 0)
+('Безлимитный', 5, -1, 0),
+('Детский', 2, 6, 1),
+('Обычный',0, 0, 0.5)
 
 SELECT * FROM lab4.Tariffs
 GO
 
-CREATE FUNCTION lab4.GetBestTariff(@usedMins int)
+CREATE FUNCTION lab4.GetBestTariff(@usedMins float)
 RETURNS @retResult TABLE(
     DesiredMinutes int,
     BestTariff nvarchar(50),
@@ -71,6 +74,7 @@ BEGIN
     FROM (
         SELECT tariff.TariffName,
             CASE
+                WHEN tariff.IncludedTime<0 THEN tariff.BasePayment
                 WHEN @usedMins-tariff.IncludedTime<0 THEN tariff.BasePayment
                 ELSE tariff.BasePayment+(@usedMins-tariff.IncludedTime)*tariff.OverCost
             END as TotalCost
@@ -98,15 +102,15 @@ AS
 BEGIN
     DECLARE @monthInMinutes int = 43200;
     DECLARE @segmentStart int = 1;
-    DECLARE @currentMinutes int = 1;
+    DECLARE @currentMinutes float = 1.5;
     DECLARE @currentBestTariff nvarchar(50) = (SELECT BestTariff FROM lab4.GetBestTariff(@currentMinutes))
     WHILE @currentMinutes<=@monthInMinutes
     BEGIN
         DECLARE @bestTariff nvarchar(50) = (SELECT BestTariff FROM lab4.GetBestTariff(@currentMinutes));
         IF @bestTariff!=@currentBestTariff
-        BEGIN  
+        BEGIN
             INSERT INTO @division VALUES
-            (@segmentStart, @currentMinutes-1, @currentBestTariff);
+            (@segmentStart, ROUND(@currentMinutes-0.5,2,1), @currentBestTariff);
             SET @currentBestTariff=@bestTariff;
             SET @segmentStart=@currentMinutes;
         END
